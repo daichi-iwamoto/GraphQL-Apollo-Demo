f28c2e1b23f12fea932a8ffff154476f1ad630cc
daichi.iwamoto <daichi.iwamoto@PC-096.local>
2021-10-23 23:17:48 +0900

    データの追加・一覧の取得

--- index.js
+++ index.js
@@ -3,23 +3,39 @@ const { ApolloServer } = require('apollo-server');
 const typeDefs = `
   type Query {
     totalMembers: Int!
+    getMembers: [Member!]!
   }
 
   type Mutation {
-    addMember(name: String! age: Int!): Boolean!
+    addMember(name: String! age: Int!): Member!
+  }
+
+  type Member {
+    id: ID!
+    name: String!
+    age: Int!
   }
 `
 
+let _id = 0
 let members = []
+
 const resolvers = {
   Query: {
-    totalMembers: () => members.length
+    totalMembers: () => members.length,
+    getMembers: () => members
   },
   
   Mutation: {
     addMember(parent, args) {
-      members.push(args)
-      return true
+
+      let newMember = {
+        id: _id++,
+        ...args
+      }
+      members.push(newMember)
+
+      return newMember
     }
   }
 }
